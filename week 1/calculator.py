462ad71fcb785f241e96beef8846bc2040d051a8
Rancho1634 <156818047+Rancho1634@users.noreply.github.com>
2024-02-03 15:07:02 +0000

    Add files via upload

---
+++ week 1/calculator.py
@@ -0,0 +1,32 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Can't divide by zero"
+    else:
+        return num1 / num2
+    
+def calculator():
+    print("Select an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice == "2":
+        num1 = int(input("Enter number: "))
+        num2 = int(input("Enter number: "))
+        print(subtract(num1, num2))
+
+    
+calculator()
+
---
+++ week 1/light_switch.py
@@ -0,0 +1,10 @@
+def toggle_light(light_on: bool):
+    if light_on:
+        light_on = False
+        print("Light truned off")
+    else:
+        light_on = True
+        print("Light turned on")
+
+    toggle_light(False)
+    
